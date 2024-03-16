f08306dd19a22cefd7f5bc3bfa687eefee40bae3
Roman Hrynkiv <rgrinkiv030924@gmail.com>
2024-03-16 18:28:00 +0200

    fixed mobile view

--- src/components/AuthFlow/AuthWrapper/styles.ts
+++ src/components/AuthFlow/AuthWrapper/styles.ts
@@ -3,6 +3,9 @@ import styled from "styled-components"
 export const Wrapper = styled.div`
     width: 100%;
     max-width: 400px;
+    @media(max-width: 500px){
+        padding: 0px 10px;
+    }
 `
 export const GlobalWrapper = styled.div`
     width: 100%;
